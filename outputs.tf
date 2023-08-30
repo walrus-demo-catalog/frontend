44fd6e1921073473fff2ffa902bf7dded2e5f079
Yinlin Li <yinlin@seal.io>
2023-08-31 00:56:10 +0800

    Test outputs

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "service_name" {
   description = "Service name"
   value       = var.service_name
 }
-
-output "service_ip" {
-  description = "Service IP"
-  value       = data.kubernetes_service.service.spec.0.cluster_ip
-}
