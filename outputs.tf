f5ed0e6d70efd8b8772c9cbea377f7ff3d981af2
Yinlin Li <yinlin@seal.io>
2024-04-10 15:32:32 +0800

    fix: service output

--- outputs.tf
+++ outputs.tf
@@ -3,14 +3,12 @@ locals {
     for node in data.kubernetes_nodes.node.nodes :
     node.status.addresses.address if node.status.addresses.type == "ExternalIP"
   ]
-
+  
+  port = data.kubernetes_service.service.spec.0.port.0.node_port
+ 
   endpoints = {
-    url = "http://${local.external_ips[0]}:"
+    url = "http://${local.external_ips[0]}:${local.port}"
   }
-}
-
-output "debug_port_data" {
-  value = data.kubernetes_service.service.spec.port
 }
 
 output "service_name" {
